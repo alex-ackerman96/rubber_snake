384a7dac71f96a2b582ca488b206f7e7b3dc8ff5
alex-a-avicena <156715519+alex-a-avicena@users.noreply.github.com>
2024-07-31 15:01:59 -0700

    copies files from source directory

--- texdoc.py
+++ texdoc.py
@@ -1,11 +1,12 @@
 import os
+import shutil
 import subprocess
 import json
 from typing import Union
 
 class Document():
     
-    def __init__(self, template = None, documentclass : str = "report", font : float = 12, columnformat : str = None, sheetsize : str = "letterpaper"):
+    def __init__(self, template = None, documentclass : str = "report", font : float = 12, columnformat : str = None, sheetsize : str = "letterpaper", sourcedir : str = None):
         
         if template != None:
             try:
@@ -42,29 +43,71 @@ class Document():
         else:
             self.documenthead = f"\documentclass[{self.font}pt, {self.sheetsize}]{{{self.documentclass}}}"
         
-        
-        self.documentstart = f"\\begin{{document}}"
-        self.documentend = f"\end{{document}}"
-        
-        self.content = [self.documenthead, self.documentstart, f"\section{{ghghghghgh}}", self.documentend]
+        if sourcedir == None:
+            self.sourcedir = os.getcwd()
+        else:
+            self.sourcedir = sourcedir
+            
+        self.projectpath = os.getcwd()
+            
+        self.packages = []
+        self.content = [f"\section{{Introduction}}"]
     
     def load_json(self, filename):
         with open(filename, 'r') as file:
             data = json.load(file)
         return data
-            
+    
+    def add(self, item):
+        if type(item) == self.Package:
+            self.packages.append(item)
+        else:
+            self.content.append(item)
+     
+    def new_figure(self, imgfile : str, label : str = None, caption : str = None, options : Union[str, list, dict] = None):
+        
+        figure = self.Figure(imgfile, label, caption, options, sourcedir = self.sourcedir, figdir = os.path.join(self.projectpath, "figures"))
+        return figure
+               
+    def create_directory(self, path : str = None, dirname : str = "New_TeX_Project"):
+
+        if path == None:
+            path = os.getcwd()
+
+        fullpath = os.path.join(path, dirname)
+        figspath = os.path.join(fullpath, "figures")
+        
+        self.projectpath = fullpath
+
+        # Check if the directory exists
+        if not os.path.isdir(fullpath):
+            # Create the directory
+            os.makedirs(fullpath)
+            print(f"Directory '{fullpath}' created.")
+            if not os.path.isdir(figspath):
+                # Create the directory
+                os.makedirs(figspath)
+                print(f"Directory '{figspath}' created.")
+        else:
+            print(f"Directory '{fullpath}' already exists.")
+    
     def save_to_file(self, filename : str = "main"):
         
         filename += ".tex"
         self.filename = filename 
         with open(filename, 'w') as file:
-            for line in self.content:
-                file.write(line + "\n")
+            file.write(f"{self.documenthead}\n")
+            for i in range(len(self.packages)):
+                file.write(self.packages[i] + "\n")
+            file.write(f"\\begin{{document}}\n")
+            for i in range(len(self.content)):
+                file.write(self.content[i] + "\n")
+            file.write(f"\\end{{document}}")   
 
     def save_to_directory(self, path : str = None, dirname : str = "New_TeX_Project"):
 
         if path == None:
-            path = os.getcwd()
+            path = self.projectpath
 
         fullpath = os.path.join(path, dirname)
         figspath = os.path.join(fullpath, "figures")
@@ -86,7 +129,7 @@ class Document():
         self.texfile = os.path.join(self.projectpath, "main")
         self.save_to_file(self.texfile)
         
-
+        
     def compile(self, miktex_path):
         os.environ["PATH"] += os.pathsep + miktex_path
         # Run pdflatex
@@ -117,8 +160,50 @@ class Document():
             
             return super().__new__(self, content)
         
-    class Figure():
+    class Equation():
         pass
+        
+    class Figure():
+        
+        def __new__(self, imgfile : str, label : str = None, caption : Union[str, list, dict] = None, options : Union[str, list, dict] = None, sourcedir = None, figdir = None):
+            
+            if sourcedir == None:
+                self.sourcedir = os.getcwd()
+            else:
+                self.sourcedir = sourcedir
+                
+            try:
+                os.makedirs(figdir, exist_ok=True)
+                img = os.path.join(figdir, os.path.basename(imgfile))
+                shutil.copy(os.path.join(sourcedir, os.path.basename(imgfile)), img)
+            except Exception as e:
+                pass
+            
+            alignment = "\\centering"
+            
+            self.content = {    
+                                "begin":f"\\begin{{figure}}\n    ",
+                                "alignment":f"{alignment}\n",
+                                "graphics":"",
+                                "caption":"",
+                                "label":"",
+                                "end":f"\\end{{figure}}"
+                            }
+            
+            width = 10
+            
+            self.content.update({"graphics":f"        \\includegraphics[width={width}cm]{{figures/{imgfile}}}\n"})
+            
+            outstr = ""
+            
+            outstr += self.content["begin"]
+            outstr += self.content["alignment"]
+            outstr += self.content["graphics"]
+            outstr += self.content["caption"]
+            outstr += self.content["label"]
+            outstr += self.content["end"]
+            
+            return outstr
     
     class Table():
         pass
@@ -126,12 +211,15 @@ class Document():
           
 if __name__ == "__main__":
     # doc = Document(columnformat="twocolumn")
-    doc = Document(template="templates/whitepaper")
-    
-    print(doc.documenthead)
-    print(doc.documentstart)
-    print(doc.documentend)
-    package = Document.Package("xcolor", ["table","xcdraw"])
-    print(package)
-    doc.save_to_directory()
-    doc.compile("C:\\Users\\aackerman\\AppData\\Local\\Programs\\MiKTeX\\miktex\\bin\\x64")
+    doc = Document(template="templates/whitepaper", sourcedir="sample")
+    doc.create_directory()
+    fig1 = doc.new_figure("Wavelength.png")
+    # package1 = Document.Package("xcolor", ["table","xcdraw"])
+    # doc.add(package1)
+    # package2 = Document.Package("graphicx")
+    # doc.add(package2)
+    # print(doc.packages)
+    # image = Document.Figure("Wavelength.png")
+    # doc.add(image)
+    # doc.save_to_directory()
+    # doc.compile("C:\\Users\\aackerman\\AppData\\Local\\Programs\\MiKTeX\\miktex\\bin\\x64")
