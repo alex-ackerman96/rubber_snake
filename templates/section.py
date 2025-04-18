5f8a866dcbf6281fbd97ef50da09586df9faad5b
alex-a-avicena <156715519+alex-ackerman96@users.noreply.github.com>
2025-04-18 13:11:50 -0700

    add section class

--- templates/section.py
+++ templates/section.py
@@ -2,14 +2,15 @@ import docx2txt
 
 
 class Section:
-    def __init__(self, title: str = None, file = filepath):
+    def __init__(self, title: str = None, file: str = None):
+        self.filepath = file
         self.title = title
-        self.content = content
+        self.content = docx2txt.process("sample/sections/Section 1.docx")
 
     def __str__(self):
         return f"{self.title}\n{self.content}"
 
 
-text = docx2txt.process("sample/sections/Section 1.docx")
-print(text)
+# text = docx2txt.process("sample/sections/Section 1.docx")
+# print(text)
 
